4b7879990ccd020499ec44dd4e73dba5f64a2774
HugoDPSz <128089628+HugoDPSz@users.noreply.github.com>
2024-03-13 14:44:08 -0300

    Add files via upload

--- buscaBinaria.py
+++ buscaBinaria.py
@@ -1,15 +1,5 @@
 import random
 
-def insertionSort(lista):
-    n = len(lista)
-    for i in range(1, n):
-        chave = lista[i]
-        j = i - 1
-        while j >= 0 and lista[j] > chave:
-            lista[j+1] = lista[j]
-            j = j - 1
-        lista[j+1] = chave
-
 def buscaBinaria(lista, alvo, inicio, fim):
     if inicio <= fim:
         teste = (inicio + fim)// 2
@@ -23,7 +13,7 @@ def buscaBinaria(lista, alvo, inicio, fim):
 
 
 lista = random.sample(range(0,100000), 10000)
-insertionSort(lista)
+lista.sort
 alvo = random.randint(0, 100000)
 inicio = 0
 fim = len(lista)
--- buscaSequencialOtimizada.py
+++ buscaSequencialOtimizada.py
@@ -1,15 +1,5 @@
 import random
 
-def insertionSort(lista):
-    n = len(lista)
-    for i in range(1, n):
-        chave = lista[i]
-        j = i - 1
-        while j >= 0 and lista[j] > chave:
-            lista[j+1] = lista[j]
-            j = j - 1
-        lista[j+1] = chave
-
 def buscaSequencial(lista, alvo, n):
     i = 0
     while alvo != lista[i] and i<n:
@@ -24,7 +14,7 @@ def buscaSequencial(lista, alvo, n):
 
 lista = random.sample(range(0,100000), 10000)
 alvo = random.randint(0, 100000)
-insertionSort(lista)
+lista.sort
 n = len(lista)
 resultado = buscaSequencial(lista, alvo, n)
 if resultado is None:
